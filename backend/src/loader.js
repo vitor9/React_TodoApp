0598a2241426e420f1b0b3ab4c96041cf100ba72
VitorSilvaSouza <vitorcontato.ti@gmail.com>
2018-07-02 17:59:31 -0300

    S4 A39 Testando a API. Testando a API utilizando o programa Postman.

--- backend/src/loader.js
+++ backend/src/loader.js
@@ -1,5 +1,3 @@
 const server = require('./config/server')
 require('./config/database')
-// Passando o server e invocando o metodo
-// Funcao exportada pelo module.export de routes.js
 require('./config/routes')(server)
