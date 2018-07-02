d4bfb1d415beb16b04290076220d0b9cafdc967d
VitorSilvaSouza <vitorcontato.ti@gmail.com>
2018-07-02 17:27:11 -0300

    S3 A38 Mapeamento de Rotas. Realizado o mapeamento das rotas.

--- backend/src/loader.js
+++ backend/src/loader.js
@@ -1,2 +1,5 @@
-require('./config/server')
+const server = require('./config/server')
 require('./config/database')
+// Passando o server e invocando o metodo
+// Funcao exportada pelo module.export de routes.js
+require('./config/routes')(server)
