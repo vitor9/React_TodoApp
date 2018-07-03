1c8c4b3dba192a20dd67d8321cac1b67232731f0
VitorSilvaSouza <vitorcontato.ti@gmail.com>
2018-07-03 10:52:05 -0300

    S5 A46 Componentes Todo e About.

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,10 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
 
 export default props => (
-  // No React, devemos substituir class por className
     <div className='container'>
-        <h1>Teste</h1>
+        <Todo />
+        <About />
     </div>
 )
