5e339f10909c1e1c693d3e41b43b15932c4cfe7d
VitorSilvaSouza <vitorcontato.ti@gmail.com>
2018-07-04 14:28:58 -0300

    S5 A58 Consulta e exclusao de tarefas. Criado as funcionalidades de consula e exclusao.

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        // Se a lista no props esta settada/existe
+        // eh guardado a lista na constante.
+        // Se nao tiver, colocamos um array vazio
+        // para garantirmos nao chamarmos um metodo
+        const list = props.list || []
+        return (
+            list.map(todo => (
+                <tr key={todo._id}>
+                    <td>{todo.description}</td>
+                    <td>
+                        <IconButton style='danger' icon='trash-o'
+                            onClick={() => props.handleRemove(todo)}></IconButton>
+                    </td>
+                </tr>
+                    ))
+                )
+            }
+        
+            return (
+        <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Descrição</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {renderRows()}
+                        </tbody>
+                    </table>
+                    )
+}
